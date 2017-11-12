08025fb57c6367723b72c443ebf0561983f4e17a
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-11-12 10:00:55 -0800

    Add files via upload

---
+++ Private/Get-CMBuildCleanXML.ps1
@@ -0,0 +1,64 @@
+<#
+.SYNOPSIS
+Scrub XML data
+
+.DESCRIPTION
+Scrub template XML data to force user to manually update values
+
+.PARAMETER XmlData
+XML data obtained from source template
+
+.EXAMPLE
+[xml]$xdata = (New-Object System.Net.WebClient).DownloadString($Source1)
+$newxml = Get-CMBuildCleanXML -XmlData $xdata
+$newxml.Save('myfile.xml')
+
+.NOTES
+General notes
+#>
+function Get-CMBuildCleanXML {
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory=$True)]
+        [xml] $XmlData
+    )
+    [xml]$result = $XmlData
+    $DefPath = '__PATH__'
+    $DefSADate = (Get-Date -f 'yyyy-M-dd 00:00:00.000')
+    Write-Verbose "clearing source path values"
+    try {
+        $result.configuration.sources.source | %{$_.path = $DefPath}
+        Write-Verbose "clearing project values"
+        $result.configuration.project.hostname='__FQDNHOSTNAME__'
+        $result.configuration.project.host='__HOST__'
+        $result.configuration.project.sitecode='___'
+        $result.configuration.project.comment='SCCM site server configuration for __NAME__, 1.0.0 by __YOU__'
+        Write-Verbose "clearing payload path references"
+        ($result.configuration.payloads.payload | ?{$_.name -eq 'SQLSERVER'}).params = '/Configuration=__PATH__\sqlsetup.ini'
+        ($result.configuration.payloads.payload | ?{$_.name -eq 'CONFIGMGR'}).params = '/script __PATH__\cmsetup.ini'
+        ($result.configuration.payloads.payload | ?{$_.name -eq 'ADK'}).params = '/installpath __PATH__ /Features OptionId.DeploymentTools OptionId.WindowsPreinstallationEnvironment OptionId.ImagingAndConfigurationDesigner OptionId.UserStateMigrationTool /norestart /quiet /ceip off'
+        Write-Verbose "clearing file values"
+        (($result.configuration.files.file | ?{$_.pkg -eq 'CONFIGMGR'}).keys.key | ?{$_.name -eq 'ProductID'}).value = 'EVAL'
+        (($result.configuration.files.file | ?{$_.pkg -eq 'CONFIGMGR'}).keys.key | ?{$_.name -eq 'SAExpiration'}).value = "$DefSADate"
+        (($result.configuration.files.file | ?{$_.pkg -eq 'CONFIGMGR'}).keys.key | ?{$_.name -eq 'SMSInstallDir'}).value = $DefPath
+        (($result.configuration.files.file | ?{$_.pkg -eq 'CONFIGMGR'}).keys.key | ?{$_.name -eq 'PrerequisitePath'}).value = $DefPath
+        ($result.configuration.files.file | ?{$_.pkg -eq 'CONFIGMGR'}).path = $DefPath
+        ($result.configuration.files.file | ?{$_.pkg -eq 'SQLSERVER'}).path = $DefPath
+        Write-Verbose "clearing local accounts properties"
+        ($result.configuration.localaccounts.localaccount | ?{$_.name -eq 'CONTOSO\sql-svc'}).name = '__SERVICEACCOUNT__'
+        ($result.configuration.localaccounts.localaccount | ?{$_.name -eq 'CONTOSO\sccmadmin'}).name = '__SCCMADMINACCOUNT__'
+        ($result.configuration.localaccounts.localaccount | ?{$_.name -eq 'CONTOSO\IT sccm admins'}).use = '0'
+        ($result.configuration.localaccounts.localaccount | ?{$_.name -eq 'CONTOSO\IT sccm admins'}).name = '__ANOTHERADMINGROUP__'
+        ($result.configuration.files.file | ?{$_.pkg -eq 'SQLSERVER'}).keys.key | ? {$_.name -match "dir"} | % {$x = $_.value ; $_.value = '__DRIVE__:'+$x.substring(3)}
+        ($result.configuration.files.file | ?{$_.pkg -eq 'SQLSERVER'}).keys.key | ? {$_.name -match "ADMINACCOUNTS"} | % {$_.value = '__ACCOUNT1__,__ACCOUNT2__'}
+        ($result.configuration.files.file | ?{$_.pkg -eq 'SQLSERVER'}).keys.key | ? {$_.name -match "SVCACCOUNT"} | %{$_.value = '__ACCOUNTNAME__'}
+        ($result.configuration.files.file | ?{$_.pkg -eq 'SQLSERVER'}).keys.key | ? {$_.name -match "SVCACCT"} | %{$_.value = '__ACCOUNTNAME__'}
+        ($result.configuration.files.file | ?{$_.pkg -eq 'SQLSERVER'}).keys.key | ? {$_.name -match "PASSWORD"} | %{$_.value = '__PASSWORD__'}
+        $result.configuration.folders.folder | %{$x = $_.name; $_.name = '__DRIVE__:'+$x.substring(3)}
+        $result.configuration.regkeys.regkey | %{$_.use = '0'}
+    }
+    catch {
+        Write-Error $_.Exception.Message
+    }
+    Write-Output $result
+}
---
+++ Private/Get-CMSiteConfigCleanXML.ps1
@@ -0,0 +1,81 @@
+<#
+.SYNOPSIS
+Scrub XML data
+
+.DESCRIPTION
+Scrub template XML data to force user to manually update values
+
+.PARAMETER XmlData
+XML data obtained from source template
+
+.EXAMPLE
+[xml]$xdata = (New-Object System.Net.WebClient).DownloadString($Source1)
+$newxml = Get-CMSiteConfigCleanXML -XmlData $xdata
+$newxml.Save('myfile.xml')
+
+.NOTES
+General notes
+#>
+function Get-CMSiteConfigCleanXML {
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory=$True)]
+        [xml] $XmlData
+    )
+    Write-Verbose "clearing source path values"
+    try {
+        [xml]$result = $XmlData
+        $result.configuration.comment = 'SCCM site ___ configuration template for __HOSTNAME__, 1.0.0 by __YOU__'
+        Write-Verbose "clearing discovery options"
+        $result.configuration.cmsite.discoveries.discovery | %{$_.use='0'}
+        $result.configuration.cmsite.discoveries.discovery | ?{$_.options -match 'contoso'} | %{$x = $_.options; $_.options = $x -replace('contoso','______')}
+        Write-Verbose "clearing boundary group properties"
+        $result.configuration.cmsite.boundarygroups.boundarygroup | %{$_.SiteSystemServer='__HOSTNAME__'}
+        $result.configuration.cmsite.boundarygroups.boundarygroup | %{$_.name='__NAME__'}
+        $result.configuration.cmsite.boundarygroups.boundarygroup | %{$_.comment='__COMMENT__'}
+        $result.configuration.cmsite.boundaries.boundary | %{$_.Use='0'}
+        $result.configuration.cmsite.boundaries.boundary | %{$_.name='__NAME__'}
+        $result.configuration.cmsite.boundaries.boundary | %{$_.value='__IPRANGE__'}
+        $result.configuration.cmsite.boundaries.boundary | %{$_.comment='__COMMENT__'}
+        $result.configuration.cmsite.boundaries.boundary | %{$_.boundarygroup='__GROUPNAME__'}
+        ($result.configuration.cmsite.clientsettings.clientsetting).settings.setting | %{$_.use = '0'}
+        Write-Verbose "clearing site system role properties"
+        (($result.configuration.cmsite.sitesystemroles.sitesystemrole | ?{$_.name -eq 'mp'}).roleoptions.roleoption | ? {$_.name -eq 'PublicFqdn'}).params = '__HOSTNAME__'
+        (($result.configuration.cmsite.sitesystemroles.sitesystemrole | ?{$_.name -eq 'ssrp'}).roleoptions.roleoption | ? {$_.name -eq 'DatabaseServerName'}).params = '__HOSTNAME__'
+        (($result.configuration.cmsite.sitesystemroles.sitesystemrole | ?{$_.name -eq 'ssrp'}).roleoptions.roleoption | ? {$_.name -eq 'DatabaseName'}).params = 'CM_XXX'
+        (($result.configuration.cmsite.sitesystemroles.sitesystemrole | ?{$_.name -eq 'ssrp'}).roleoptions.roleoption | ? {$_.name -eq 'UserName'}).params = '__ACCOUNTNAME__'
+        (($result.configuration.cmsite.sitesystemroles.sitesystemrole | ?{$_.name -eq 'acwp'}).roleoptions.roleoption | ? {$_.name -eq 'OrganizationName'}).params = '__ORGNAME__'
+        (($result.configuration.cmsite.serversettings.serversetting | ?{$_.name -eq 'CMSoftwareDistributionComponent'})).value = '__ACCOUNTNAME__'
+        ($result.configuration.cmsite.clientoptions.CMClientPushInstallation).Accounts = '__ACCOUNT__,__ACCOUNT__'
+        ($result.configuration.cmsite.clientoptions.CMClientPushInstallation).InstallationProperty = 'SMSSITECODE=___'
+        Write-Verbose "clearing client settings"
+        ($result.configuration.cmsite.clientsettings.clientsetting.settings.setting.options.option | ?{$_.name -eq 'PortalURL'}).value = '__PORTALURL__'
+        Write-Verbose "clearing distribution point settings"
+        ($result.configuration.cmsite.dpgroups.dpgroup) | %{$_.name='__DPGROUPNAME__'}
+        ($result.configuration.cmsite.dpgroups.dpgroup) | %{$_.comment='__COMMENT__'}
+        ($result.configuration.cmsite.dpgroups.dpgroup)[0].name='All DP Servers'
+        ($result.configuration.cmsite.dpgroups.dpgroup)[0].comment='All Distribution Point Servers'
+        ($result.configuration.cmsite.dpgroups.dpgroup)[1].name='All PXE DP Servers'
+        ($result.configuration.cmsite.dpgroups.dpgroup)[1].comment='All PXE-enabled Distribution Point Servers'
+        Write-Verbose "clearing site system role settings"
+        ($result.configuration.cmsite.sitesystemroles.sitesystemrole) | %{$_.roleoptions.roleoption} | ? {$_.name -eq 'AddProducts'} | %{$_.use ='0'}
+        Write-Verbose "clearing os images and os installer properties"
+        $result.configuration.cmsite.osimages.osimage | %{$_.path = '__PATH_TO_WIM_FILE__'}
+        $result.configuration.cmsite.osimages.osimage | %{$_.use = '0'}
+        $result.configuration.cmsite.osinstallers.osinstaller | %{$_.path = '__PATH_TO_EXE_FILE__'}
+        $result.configuration.cmsite.osinstallers.osinstaller | %{$_.use = '0'}
+        Write-Verbose "clearing maintenance task properties"
+        ($result.configuration.cmsite.mtasks.mtask | ?{$_.name -match 'Backup'}).options='__PATH__'
+        Write-Verbose "clearing application and deployment types properties"
+        ($result.configuration.cmsite.applications.application).deptypes.deptype | %{$x = $_.source;$_.source = $x.replace('FS1\apps','__PATH__')}
+        Write-Verbose "clearing site system accounts properties"
+        $result.configuration.cmsite.accounts.account | %{$_.name = '__ACCOUNTNAME__'}
+        $result.configuration.cmsite.accounts.account | %{$_.password = '__PASSWORD__'}
+        Write-Verbose "clearing EPP policy template properties"
+        $result.configuration.cmsite.malwarepolicies.malwarepolicy | %{$x = $_.path; $_.path = $x.replace('E:\CONFIGMGR','__PATH__')}
+    }
+    catch {
+        Write-Error $_.Exception.Message
+    }
+    Write-Output $result
+}
