4ee6ccf43a455f5887d2be39b07dd4e78065d840
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-11-16 02:54:30 -0500

    Add files via upload

---
+++ Private/Get-CMxTemplateScrubData.ps1
@@ -0,0 +1,19 @@
+function Get-CMxTemplateScrubData {
+    param (
+		[parameter(Mandatory=$True)]
+		$XmlData
+    )
+	try {
+		Write-Verbose "scrubbing template data"
+		if (-not $NoScrub) {
+			[xml]$result = Get-CMBuildCleanXML -XmlData $XmlData
+		}
+		else {
+			[xml]$result = $xmldata
+		}
+	}
+	catch {
+		Write-Error $_.Exception.Message
+	}
+	Write-Output $result
+}
