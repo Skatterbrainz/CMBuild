5197b86f57975544af56be84d20c62a1c709a5bc
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-01-22 11:25:26 -0500

    Feature update
    Added support for using @ORGNAME@ in within the Query name to support inline replacement using $orgname.  For example "@ORGNAME@ - MAC and Serial Numbers" turns into "Contoso - MAC and Serial Numbers" when processed.

--- Private/Import-CmxQueries.ps1
+++ Private/Import-CmxQueries.ps1
@@ -1,11 +1,27 @@
 function Import-CmxQueries {
-    [CmdletBinding(SupportsShouldProcess=$True)]
+    <#
+    .SYNOPSIS
+    Create ConfigMgr Custom Queries from XML data input
+    
+    .DESCRIPTION
+    Create ConfigMgr Custom Queries from XML data input
+    
+    .PARAMETER DataSet
+    XML data set
+    
+    .EXAMPLE
+    Import-CmxQueries -DataSet $xmlData
+    
+    .NOTES
+    ...
+    #>
+    [CmdletBinding()]
     param (
-        [parameter(Mandatory=$True)]
+        [parameter(Mandatory=$True, HelpMessage="XML Data Set")]
         [ValidateNotNullOrEmpty()]
-        $DataSet
+        [xml] $DataSet
     )
-	Write-Log -Category "info" -Message "------------------------------ Import-CmxQueries -------------------------------"
+	Write-Log -Category "info" -Message "------------------------------ Import-CmxQueries -------------------------------" -LogFile $logfile
     Write-Host "Importing custom Queries" -ForegroundColor Green
     $result = $True
     $Time1  = Get-Date
@@ -14,22 +30,28 @@ function Import-CmxQueries {
         $queryComm = $item.comment
         $queryType = $item.class
         $queryExp  = $item.expression
+        if ($orgname -and ($orgname.length -gt 0)) {
+            $NewName = $queryName -replace '@ORGNAME@', $orgname
+        }
+        else {
+            $NewName = $queryName
+        }
         try {
-            New-CMQuery -Name $queryName -Expression $queryExp -Comment $queryComm -TargetClassName $queryType | Out-Null
-            Write-Log -Category "info" -Message "item created successfully: $queryName"
+            New-CMQuery -Name $NewName -Expression $queryExp -Comment $queryComm -TargetClassName $queryType | Out-Null
+            Write-Log -Category "info" -Message "item created successfully: $NewName" -LogFile $logfile
         }
         catch {
             if ($_.Exception.Message -like "*already exists*") {
-                Write-Log -Category "info" -Message "item already exists: $queryname"
+                Write-Log -Category "info" -Message "item already exists: $NewName" -LogFile $logfile
             }
             else {
-                Write-Log -Category "error" -Message $_.Exception.Message
+                Write-Log -Category "error" -Message $_.Exception.Message -Severity 3 -LogFile $logfile
                 $result = $False
                 break
             }
         }
-        Write-Verbose "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+        Write-Log -Category "info" -Message "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -" -LogFile $logfile
     } # foreach
-    Write-Log -Category info -Message "function runtime: $(Get-TimeOffset $time1)"
+    Write-Log -Category info -Message "function runtime: $(Get-TimeOffset $time1)" -LogFile $logfile
     Write-Output $result
 }
